1395312e32e9fb5eefa78d91a1aebb688708b9c3
chris <danteschrisanmae@yahoo.com>
2020-11-14 15:35:14 +0800

    added

--- main/main/urls.py
+++ main/main/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, re_path
 from students import views
@@ -20,5 +21,5 @@ from students import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/students/$', views.students_list),
-    re_path(r'^api/students/(?P<pk>\d+)/$', views.students_detail),
+    url(r'^api/students/(?P<pk>\d+)$', views.students_detail),
 ]
